9f27ec518e74b85acac338ead75768e85b9b19e0
kgyyds <1361906712@qq.com>
2026-01-21 19:09:17 +0800

    更改图标

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kgapp.encryptionchat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20
+        versionName = "2.0"
     }
 
     buildTypes {
